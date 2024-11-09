78e45a8622bdb9739b4b7367baa6ed83af46bafd
HongYoungChul <129755423+HongYoungChul@users.noreply.github.com>
2024-11-09 11:52:08 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,14 +1,2 @@
-import sys
 
-def find_divisors(n):
-    divisors = [i for i in range(1, n + 1) if n % i == 0]
-    return divisors
-
-if __name__ == "__main__":
-    try:
-        # 여러 줄 입력을 받아 각 줄을 처리
-        for line in sys.stdin:
-            num = int(line.strip())
-            print(", ".join(map(str, find_divisors(num))))
-    except ValueError:
-        print("Please enter a valid integer.")
+print("Hello")
