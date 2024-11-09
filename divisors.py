0f01af85d5297637f4afc87d70fd770d15a80202
HongYoungChul <129755423+HongYoungChul@users.noreply.github.com>
2024-11-09 11:58:02 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,4 +1,14 @@
-# 디버그 메시지를 여러 줄로 출력
-print("Debug: Starting program")
-print("Hello")
-print("Debug: End of program")
+import sys
+
+def find_divisors(n):
+    # n의 약수를 구하는 함수
+    divisors = [i for i in range(1, n + 1) if n % i == 0]
+    return divisors
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        # 명령줄 인자로부터 숫자를 받아 약수를 구하고 출력
+        num = int(sys.argv[1])
+        print(" ".join(map(str, find_divisors(num))))
+    else:
+        print("Please provide a number as a command line argument.")
